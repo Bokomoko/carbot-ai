aef2fb15e22aa142f39756ae87610fd3c43e9294
João Eurico Bokomoko Lima <34620239+Bokomoko@users.noreply.github.com>
2022-07-04 17:50:44 +0000

    Comments added to script.js

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ canvas.width = 200               // sets the width (horizontal size)
 
 const ctx = canvas.getContext("2d")  // the context that we'll draw things to
 
-const road = new Road( canvas.width/2, canvas.width*0.9, 4)
+// @Road( position of the center of the road, width of the road, number of lanes)
+const road = new Road( canvas.width/2, canvas.width*0.9, 3)
+// lanes are numbered from 0 to laneCount -1 
+// get the center of the middle lane
 const middle = road.getLaneCenter(Math.floor(road.laneCount/2))
-const car = new Car(middle,100,30,50)   // creates a car at pos 100,100 with 30x50 pixels size
+// create a car and put it in this middle lane
+const car = new Car(middle,100,30,50)   // creates a car at the middle lane of the road
 
 car.draw(ctx) // invokes the method to draw the car into a context
 
